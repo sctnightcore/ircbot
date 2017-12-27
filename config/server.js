85900a034255a04758756931a6fc2211e34944cd
Maytichai Saowa <nightcoremax@gmail.com>
2017-12-28 02:17:36 +0700

    Update server.js

--- config/server.js
+++ config/server.js
@@ -2,8 +2,8 @@ module.exports = {
     server: 'irc.freenode.net',
     port: 6697,
     secure: true,
-    name: 'OpenKore',
-    userName: 'OpenKore',
-    realName: 'Source available via http://github.com/openkore/ircbot',
+    name: 'sctnightcore24hour',
+    userName: 'sctnightcore',
+    realName: 'XD',
     channels: ['#openkore']
 };
